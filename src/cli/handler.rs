4af76b84fefab1336484365c7c44e2770c7fe1bd
x0710-Notebook <x0710_x0710@outlook.com>
2025-12-28 16:36:31 +0800

    Optimize code structure

--- src/cli/handler.rs
+++ src/cli/handler.rs
@@ -5,7 +5,7 @@ use std::process::ExitStatus;
 use std::str::FromStr;
 use clap::Parser;
 use rustyline::error::ReadlineError;
-use tempfile::NamedTempFile;
+use time::error::Parse;
 use crate::cli::args;
 use crate::cli::args::Args;
 use crate::cli::handler::Mode::{Interactive, Once};
@@ -99,54 +99,26 @@ impl CliHandler {
     fn deal_command(&self, command: &str) -> Result<(), CliErr> {
         let (ops, arg) = command.split_once(' ').unwrap_or((command, ""));
         let ops = ops.parse::<SubCommand>()?;
-        let mut tmpfile = NamedTempFile::new()?;
 
         let date = parse_to_date(&arg);
         match ops {
             SubCommand::Add => {
-                if let Some(date) = date {
-                    if let Some(day) = self.conn.read_day(&date) {
-                        tmpfile.write_all(day.event().instruct.as_bytes())?;
-                        tmpfile.flush()?;
-                        tmpfile.seek(SeekFrom::Start(0))?;
-                    }
-                    edit_file(tmpfile.path())?;
-                        // .expect("Please Config your Default EDITOR!");
-                    tmpfile.flush()?;
-                    tmpfile.seek(SeekFrom::Start(0))?;
-                    let mut data = String::new();
-                    tmpfile.read_to_string(&mut data)?;
-                    let day = Day::new(
-                        date,
-                        Event::new(&data),
-                        None,
-                        None,
-                    );
-                    self.conn.add_day(&day)?;
-                }else {
-                    println!("Please input correct time");
-                }
+                let date = date?;
+                let initial = self.conn.read_day(&date)
+                    .map_or(String::from(""), |day| day.event().instruct.clone());
+                let initial = edit_with_editor(&initial)?;
+                let day = Day::from_event(&date, &initial.parse::<Event>().unwrap());
+                self.conn.add_day(&day)?;
             },
             SubCommand::Remove => {
-                if let Some(date) = date {
-                    self.conn.remove_day(&date)?;
-                }else {
-                    println!("Please input correct date");
-                }
+                let date = date?;
+                self.conn.remove_day(&date)?;
             },
             SubCommand::Check => {
-                if let Some(date) = date {
-                    if let Some(date) = self.conn.read_day(&date) {
-                        tmpfile.write_all(date.event().instruct.as_bytes())?;
-                        tmpfile.flush()?;
-                        tmpfile.seek(SeekFrom::Start(0))?;
-                        cat_file(tmpfile.path())?;
-                    }else {
-                        println!("There was no such day.");
-                    }
-                }else {
-                    println!("Please input correct date");
-                }
+                let date = date?;
+                let initial = self.conn.read_day(&date)
+                    .map_or(String::from(""), |day| day.event().instruct.clone());
+                println!("{}", initial);
             },
             SubCommand::ListAll => {
                 unimplemented!()
@@ -178,11 +150,36 @@ impl FromStr for Date {
     }
 }
  */
-pub fn parse_to_date(source: &str) -> Option<time::Date> {
-    let f1 = time::macros::format_description!("[year]-[month]-[day]");
-    let f2 = time::macros::format_description!("[year][month][day]");
+pub fn parse_to_date(source: &str) -> Result<time::Date, CliErr> {
+    let source = source.trim();
+    let today = time::OffsetDateTime::now_utc().date();
 
-    time::Date::parse(source, &f1).or_else(|_| time::Date::parse(source, &f2)).ok()
+    match source {
+        "yesterday" | "y" => Ok(today.previous_day().unwrap()),
+        "tomorrow" | "m" => Ok(today.next_day().unwrap()),
+        "today" | "t" => Ok(today),
+        _ => {
+            let f1 = time::macros::format_description!("[year]-[month]-[day]");
+            let f2 = time::macros::format_description!("[year][month][day]");
+            time::Date::parse(source, f1).or_else(|e| {
+                time::Date::parse(source, f2)
+            }).map_err(|e| CliErr::InvalidDate(e.to_string()))
+        }
+    }
+
+}
+fn edit_with_editor(s: &str) -> Result<String, CliErr> {
+    let mut editor = tempfile::NamedTempFile::new()?;
+    editor.write_all(s.as_bytes())?;
+    editor.flush()?;
+    editor.seek(SeekFrom::Start(0))?;
+
+    edit_file(editor.path())?;
+
+
+    let mut res = String::new();
+    editor.read_to_string(&mut res)?;
+    Ok(res)
 }
 pub fn edit_file(file: &Path) -> std::io::Result<ExitStatus> {
     std::process::Command::new(args::editor())
@@ -199,6 +196,7 @@ pub enum Mode {
     Once,
 }
 
+#[derive(Debug)]
 enum CliErr {
     Exit,
     Io(std::io::Error),
@@ -207,6 +205,11 @@ enum CliErr {
     UnknownCommand(String),
 }
 
+impl From<time::error::Parse> for CliErr {
+    fn from(value: Parse) -> Self {
+        CliErr::InvalidDate(value.to_string())
+    }
+}
 
 impl From<rusqlite::Error> for CliErr {
     fn from(err: rusqlite::Error) -> Self {
