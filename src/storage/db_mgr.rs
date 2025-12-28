c5ce3f7211860cf57c17fe5fa0ac8167d4753438
x0710-Notebook <x0710_x0710@outlook.com>
2025-12-28 17:09:27 +0800

    Optimize database functions

--- src/storage/db_mgr.rs
+++ src/storage/db_mgr.rs
@@ -2,40 +2,33 @@ use rusqlite::{params, Connection};
 use rusqlite::fallible_iterator::FallibleIterator;
 use crate::model::day::Day;
 use crate::model::event::Event;
-
+const DATE_FORMAT: &[time::format_description::FormatItem<'static>] =
+    time::macros::format_description!("[year]-[month]-[day]");
 pub struct DatabaseManager {
     conn: rusqlite::Connection,
 }
 impl DatabaseManager {
     pub fn remove_day(&self, date: &time::Date) -> Result<usize, rusqlite::Error> {
-        let res = self.conn.execute("DELETE FROM day WHERE date=?",
-        params![date.to_string()])?;
-
-        Ok(res)
+        self.conn.execute("DELETE FROM day WHERE date=?",
+        [date.format(DATE_FORMAT).unwrap()])
     }
-    pub fn read_all(&self) -> Option<Vec<Day>> {
-        unimplemented!("read all days had been recorded");
+    pub fn read_all(&self) -> Result<Vec<Day>, rusqlite::Error> {
+        let mut stmt = self.conn.prepare("SELECT date, event, weather, mood FROM day ORDER BY date DESC")?;
+        let res = stmt
+            .query_map((), |row| row_to_day(row))?;
+        res.collect()
+
     }
     pub fn read_day(&self, date: &time::Date) -> Option<Day> {
-        let format = time::format_description::parse("[year]-[month]-[day]").unwrap();
-        let res = self.conn.query_row("SELECT date,event,weather,mood FROM day WHERE date=?",
-        [date.to_string()], |row| {
-            // Formatter
-            // raw_datum
-            let date_raw: String = row.get(0)?;
-            let date = time::Date::parse(&date_raw, &format).unwrap();
-            let event_str: String = row.get(1)?;
-            let weather = row.get(2)?;
-            let mood = row.get(3)?;
-            // Obj
-            Ok(Day::new(date, Event::new(&event_str), weather, mood))
-        });
-        res.ok()
+        self.conn.query_row("SELECT date,event,weather,mood FROM day WHERE date=?",
+        params![date.format(DATE_FORMAT).unwrap()], |row|
+            row_to_day(row)
+        ).ok()
     }
     pub fn add_day(&self, day: &Day) -> Result<usize, rusqlite::Error> {
         let res = self.conn.execute(
             "INSERT OR REPLACE INTO  day (date, event, weather, mood) VALUES (?1, ?2, ?3, ?4)",
-            (day.date().to_string(),
+            (day.date().format(DATE_FORMAT).unwrap(),
              day.event().instruct.to_string(),
              day.weather(),
              day.mood()),
@@ -52,8 +45,18 @@ impl DatabaseManager {
             weather TEXT,
             mood TEXT)
         "#, ())?;
-        Ok(DatabaseManager {
+        Ok(Self {
             conn,
         })
     }
+}
+fn row_to_day(row: &rusqlite::Row) -> Result<Day, rusqlite::Error> {
+    // raw_datum
+    let date_raw: String = row.get(0)?;
+    let date = time::Date::parse(&date_raw, DATE_FORMAT).unwrap();
+    let event_str: String = row.get(1)?;
+    let weather = row.get(2)?;
+    let mood = row.get(3)?;
+    // Obj
+    Ok(Day::new(date, Event::new(&event_str), weather, mood))
 }
