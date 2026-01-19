9ab888e5b3dabb453fcab6f412c7a41fcf766bb9
x0710-Notebook <x0710_x0710@outlook.com>
2026-01-19 21:09:06 +0800

    Complete Export Function

--- core/src/storage/io/export.rs
+++ core/src/storage/io/export.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use crate::base::date::Date;
 use crate::base::error::Error;
@@ -7,14 +9,15 @@ use crate::storage::io::mode::Format;
 
 pub struct Exporter<'a> {
     db_mgr: &'a DatabaseManager,
-    path: Box<Path>,
+    path: PathBuf,
     mode: Format,
 }
 impl<'a> Exporter<'a> {
-    pub fn new(db_mgr: &'a DatabaseManager, export_path: Box<Path>, mode: Format) -> Self {
+    pub fn new(db_mgr: &'a DatabaseManager, export_path: impl AsRef<Path>, mode: Format) -> Self {
+        let path = export_path.as_ref().to_path_buf();
         Self {
             db_mgr,
-            path: export_path,
+            path,
             mode,
         }
     }
@@ -38,7 +41,10 @@ impl<'a> Exporter<'a> {
                 for record in days { csv_writer.serialize(record)?; }
                 csv_writer.flush()?;
             },
-            Format::JSON => {}
+            Format::JSON => {
+                let json = serde_json::to_string(&days).unwrap();
+                File::create(&self.path)?.write_all(json.as_bytes())?;
+            }
         }
 
         Ok(())
@@ -46,9 +52,9 @@ impl<'a> Exporter<'a> {
 }
 #[test]
 fn write_to_csv() {
-    let db_mgr = DatabaseManager::from_path(Path::new("~/.local/share/diary/diary.db")).unwrap();
-    let out = PathBuf::from("/tmp/test.csv");
-    let mut e = Exporter::new(&db_mgr, out.into_boxed_path(), Format::CSV);
+    let db_mgr = DatabaseManager::from_path(Path::new("/home/x0710/.local/share/diary/diary.db")).unwrap();
+    let out = PathBuf::from("/tmp/test.json");
+    let mut e = Exporter::new(&db_mgr, out.into_boxed_path(), Format::JSON);
     // e.from_to(Date::new(2020, 1, 1).unwrap(), Date::new(2025, 12, 12).unwrap()).unwrap();
 
     e.all_export().unwrap();
