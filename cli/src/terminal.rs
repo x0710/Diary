5eea33d90fac9dd2e2abb527a60128b58d06b75d
x0710-Notebook <x0710_x0710@outlook.com>
2026-02-24 10:25:42 +0800

    修复在交互模式下，无输入时提示无效命令

--- cli/src/terminal.rs
+++ cli/src/terminal.rs
@@ -16,7 +16,9 @@ use crate::args::{Args, Commands};
 use crate::error::CliError;
 use crate::executor::CliExecutor;
 
+/// Cli实体表示
 pub struct CliSession {
+    /// 用户启动程序时所采用的参数
     pub args: Args,
     pub(crate) executor: CliExecutor,
 }
@@ -31,15 +33,15 @@ impl CliSession {
         }
     }
     pub fn run(&mut self) {
-        if let Some(c) = self.args.command.clone() {
-            self.once(c);
+        if self.args.command.is_some() {
+            self.once();
         }else {
             self.interactive();
         }
-        
     }
-    fn once(&mut self, command: Commands) {
-        match command {
+    /// 如果用户通过命令行解析
+    fn once(&mut self) {
+        match self.args.command.as_ref().unwrap() {
             Commands::Interactive => self.interactive(),
             Commands::Import {path} => {
                 let mut imp = Importer::new(self.executor.exec.conn_mut());
@@ -63,6 +65,7 @@ impl CliSession {
             }
         }
     }
+    /// 如果用户通过交互式运行程序
     fn interactive(&self) {
         // History Enable
         let s = Config::builder().auto_add_history(true).build();
@@ -70,6 +73,7 @@ impl CliSession {
         loop {
             match rl.readline(">: ") {
                 Ok(line) => {
+                    if line.is_empty() { continue }
                     match self.executor.exec_command(&line) {
                         Ok(_) => (),
                         Err(CliError::Quit) => break,
@@ -89,6 +93,9 @@ impl CliSession {
         }
     }
 }
+/// 通过调用外部编辑器编辑文本
+/// *s* 预设文本
+/// *date* 预设日期（将在临时文件名中出现）
 pub fn edit_with_editor(s: &str, date: impl AsRef<OsStr>) -> Result<String, CliError> {
     let mut suffix = date.as_ref().to_os_string();
     // 设置临时文件为markdown格式
@@ -109,13 +116,9 @@ pub fn edit_with_editor(s: &str, date: impl AsRef<OsStr>) -> Result<String, CliE
     editor.read_to_string(&mut res)?;
     Ok(res)
 }
+/// 通过默认编辑器打开文件
 pub fn edit_file(file: impl AsRef<Path>) -> std::io::Result<ExitStatus> {
     std::process::Command::new(args::editor())
-        .arg(file.as_ref())
-        .status()
-}
-pub fn cat_file(file: impl AsRef<Path>) -> std::io::Result<ExitStatus> {
-    std::process::Command::new("cat")
         .arg(file.as_ref())
         .status()
 }
